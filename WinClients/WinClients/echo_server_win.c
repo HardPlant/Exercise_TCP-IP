ade6835f7c4604d7b007f11b037ad3e170057e1c
HardPlant <abc7988se@naver.com>
2017-02-20 19:06:26 +0900

    VS added

--- WinClients/WinClients/echo_server_win.c
+++ WinClients/WinClients/echo_server_win.c
@@ -6,12 +6,13 @@
 int main()
 {
     WSADATA wsaData;
-    SOCK listen_s, client_s;
+    SOCKET listen_s, client_s;
     struct sockaddr_in server_addr, client_addr;
     char szReceiveBuffer[MAX_PACKETLEN];
     int readn, writen;
-
-    if(WSAStartup(MAKEWORD(2,2),&wsaData !=0)) return 1;
+    int nPort = 3500;
+	HANDLE hChatServer;
+    if(WSAStartup(MAKEWORD(2,2),&wsaData) !=0) return 1;
 
     listen_s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(listen_s == INVALID_SOCKET) return 1;
@@ -26,14 +27,26 @@ int main()
         == bind(listen_s, (struct sockaddr*)&server_addr,
                 sizeof(struct sockaddr_in)))return 0;
     
-    if(SOCKET_ERROR == listen(hChatServer, 5)) return 0;
+    if(SOCKET_ERROR == listen(server_addr, 5)) return 0;
 
     ZeroMemory(&client_addr, sizeof(struct sockaddr_in));
 
     int nAcceptClientInfo = sizeof(struct sockaddr_in);
-    client_s = accept(hChatServer
-        ,(struct sockaddr*)&tagAcceptClientInfo,&nAcceptClientInfo);
+    client_s = accept(server_addr
+        ,(struct sockaddr*) &client_addr,&nAcceptClientInfo);
+    int nReceiveBytes = 0;
 
+    while(1)
+    {
+        readn = recv(hClient, szReceiveBuffer, MAX_PACKETLEN, 0);
+        if (nReceiveBytes > 0)
+        {
+            writen = send(hClient, szReceiveBuffer, nReceiveBytes, 0);
+        }
+        closesocket(client_s);
+    }
 
+    closesocket(server_s);
+    WSACleanup();
 
 }
