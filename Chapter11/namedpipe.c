796b0209badf36d7e1605cf008615c8d5a3b851b
HardPlant <abc7988se@naver.com>
2017-02-28 05:38:12 -0800

    added named pipe

--- Chapter11/namedpipe.c
+++ Chapter11/namedpipe.c
@@ -1,3 +1,20 @@
-#include <sys/types.h>
+#include <sys/types.h> // int mkfifo(char* pathname, mode_t mode);
 #include <sys/stat.h>
 
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h> //also can mknode(..,dev_t dev) // system func, make fifo
+
+#include "../err.h"
+int main(int argc, char* argv[])
+{
+    if (err_failed == mkfifo("/tmp/myfifo_r", S_IRUSR|O_RDONLY)) perr(err_one);
+
+    printf("ReadOnly named pipe : /tmp/myfifo_r\n");
+
+    if(err_failed == mkfifo("/tmp") S_IRUSR|O_WRONLY) perr(err_one);
+
+    printf("WriteOnly Named Pipe : /tmp/myfifo_w\n");
+
+    return 0;
+}
